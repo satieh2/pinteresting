12b164c3d7620128ee154e26e7a58caa94cb7fd9
Steven Atieh <steve.atieh@gmail.com>
2014-05-15 16:30:06 -0400

    Add Amazon S3 for Paperclip uploads to Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
+    # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
@@ -38,3 +38,5 @@ Rails.application.configure do
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
+
+Paperclip.options[:command_path] = "/Program Files/ImageMagick-6.8.9-Q16"
