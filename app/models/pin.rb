6f24fc130791962a8fb4d07df1972160b3451b45
Steven Atieh <steve.atieh@gmail.com>
2014-05-31 15:42:54 -0400

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,9 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+	validates :image, presence: true
+	validates :description, presence: true
 end
 
 #Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.8.9-Q16"
